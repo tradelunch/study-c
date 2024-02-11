5dc25e2fd17e4fb3de479c7aed6af62fd5bf32a6
tlim41 <tio.taek.lim@gatech.edu>
2024-02-11 16:32:00 +0900

    feat: check pattern func

---
+++ extra-6 check patter.c
@@ -0,0 +1,49 @@
+#include <string.h>
+
+/**
+ * @details check GETFILE
+ * if yes, return offset + len;
+ * if no, return -1;
+ */
+int check_pattern(char *res_header, int offset, int len, char *pattern)
+{
+
+	char *pattern_p = pattern;
+
+	int i = offset;
+	int cnt = 0;
+	while (i < offset + len)
+	{
+		printf("pattern_p: %s\n", pattern_p);
+		if (*(res_header + i) == *pattern_p)
+		{
+			pattern_p += 1;
+			cnt++;
+		}
+		else
+		{
+			pattern_p = pattern;
+			cnt = 0;
+		}
+
+		if (cnt == len)
+		{
+			return offset + len;
+		}
+
+		i++;
+	}
+
+	return 0;
+}
+
+int main(int argc, char const *argv[])
+{
+	char *res_header = "GETFILE OK 4\r\n\r\ngood";
+
+	char *GETFILE_SCHEME = "GETFILE";
+
+	int new_offset = check_pattern(res_header, 0, strlen(GETFILE_SCHEME), GETFILE_SCHEME);
+
+	return 0;
+}
